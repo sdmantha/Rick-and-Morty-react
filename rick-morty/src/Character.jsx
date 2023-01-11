6318dc08c0e17da30be871768f812742afff30a1
sdmantha <sdangelcake@gmail.com>
2023-01-11 09:48:50 -0500

    first commit, need to work on toggle more for each pic

---
+++ rick-morty/src/Character.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Character() {
+    return (
+      <div>
+        <nav>
+          <h1>Rick and Morty</h1>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Character;
---
+++ rick-morty/src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function Nav (){
+    return(
+        <nav className='TitleName'>
+            <h1> Rick and Morty</h1>
+            <button>Characters</button>
+        </nav>
+    )
+}
+
+
+export default Nav
